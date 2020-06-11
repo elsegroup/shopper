dabf24e47b771a384ed1cb84ae5b77aad77df067
SHEROZ Juraev <credocoder@gmail.com>
2020-06-11 16:41:52 +0500

    Добавление CORS в middleware

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
-
 use Illuminate\Http\Request;
 
 Route::group([
         'as' => 'api.',
         'namespace' => 'Api\v1',
+        'middleware' => ['cors', 'api']
     ], function () {
         Route::apiResource('products', 'ProductsController')->except(['update', 'store', 'destroy']);
-
         Route::apiResource('catalogs', 'CatalogsController')->except(['update', 'store', 'destroy']);
-
         Route::apiResource('carts', 'CartController')->except(['update', 'index']);
-
         Route::post('/carts/{cart}', 'CartController@addProducts');
         Route::post('/carts/{cart}/checkout', 'CartController@checkout');
-
         Route::get('pages/{page}', 'PageController');
 });
