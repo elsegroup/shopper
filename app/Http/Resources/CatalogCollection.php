a12bbf3ab408018b5803c01a1e85cd49c454565e
SHEROZ Juraev <credocoder@gmail.com>
2020-07-04 22:30:08 +0500

    CatalogCollection.php now returns only id,name, and slug

--- app/Http/Resources/CatalogCollection.php
+++ app/Http/Resources/CatalogCollection.php
@@ -14,6 +14,12 @@ class CatalogCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => [
+                'id' => $this->id,
+                'name' => $this->name,
+                'slug' => $this->slug
+            ]
+        ];
     }
 }
